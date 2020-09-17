9db4fc13d3c7d8698a18bab6b660588005ecbfd2
saul <59045995+sauldev02@users.noreply.github.com>
2020-09-17 03:22:18 -0400

    Add files via upload

---
+++ js/utility.js
@@ -0,0 +1,11 @@
+  	function openNav() {
+  	    document.getElementById("sideNavigation").style.width = "80%";
+  	    //document.getElementById("main").style.marginLeft = "250px";
+  	}
+  	 
+  	function closeNav() {
+  	    document.getElementById("sideNavigation").style.width = "0";
+  	    //document.getElementById("main").style.marginLeft = "0";
+  	}
+
+
